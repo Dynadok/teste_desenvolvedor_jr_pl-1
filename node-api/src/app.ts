6988bf5072c14b644755bbfa63c12ebf601bf7a9
igordantasgf <igordantasgf@gmail.com>
2025-01-09 00:13:20 -0300

    porta para checar funcionamento da api

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -1,8 +1,13 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import tasksRoutes from './routes/tasksRoutes';
 
 const app: Application = express();
 app.use(express.json());
+
+// Rota inicial
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "API is running" });
+});
 
 // Rotas
 app.use('/tasks', tasksRoutes);
